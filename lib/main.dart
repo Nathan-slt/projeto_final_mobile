c347ac15ec973f2f1c82235943a7023b4e5fa052
Nathan-slt <nathansenai22@gmail.com>
2026-09-02 16:07:20 -0300

    third commit
    Co-authored-by: Kauan Santos <kauansantos2git@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,13 +15,21 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'MedLink',
+      locale: const Locale('pt', 'BR'),
+      supportedLocales: const [Locale('pt', 'BR')],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       theme: ThemeData(
         fontFamily: 'Inter',
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
+          seedColor: const Color(0xFF082849),
           primary: const Color(0xFF082849),
           secondary: const Color(0XFF0371CA),
           surface: const Color(0XFFF0F7FF),
+          onSurface: const Color(0XFF082849),
         ),
       ),
       home: const SplashScreen(),
@@ -77,9 +87,140 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
         centerTitle: true,
-        title: SvgPicture.asset('assets/images/medlink.svg'),
+        title: SizedBox(
+          height: 35,
+          child: SvgPicture.asset(
+            'assets/images/medlink.svg',
+            fit: BoxFit.contain,
+          ),
+        ),
       ),
-      body: const Center(child: Text('Em construção ...')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Olá, Kauan!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Próxima consulta:',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+
+                Card(
+                  color: Colors.white,
+                  elevation: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Próxima consulta',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Nenhuma consulta agendada',
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                Calendario(),
+              ],
+            ), 
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Calendario extends StatefulWidget {
+  const Calendario({super.key});
+
+  @override
+  State<Calendario> createState() => _CalendarioState();
+}
+
+class _CalendarioState extends State<Calendario> {
+  DateTime _selectedDate = DateTime.now();
+  DateTime _focusedDate = DateTime.now();
+
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.centerRight,
+          child: IconButton(
+            icon: Icon(
+              _calendarFormat == CalendarFormat.month
+                  ? Icons.calendar_view_week
+                  : Icons.calendar_month,
+            ),
+            iconSize: 30,
+            onPressed: () {
+              setState(() {
+                _calendarFormat = _calendarFormat == CalendarFormat.month
+                    ? CalendarFormat.week
+                    : CalendarFormat.month;
+              });
+            },
+          ),
+        ),
+
+        TableCalendar(
+          locale: 'pt_BR',
+
+          firstDay: DateTime.utc(2020, 1, 1),
+          lastDay: DateTime.utc(2030, 12, 31),
+          focusedDay: _focusedDate,
+
+          headerStyle: const HeaderStyle(
+            formatButtonVisible: false,
+          ),
+
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDate, day);
+          },
+
+          calendarFormat: _calendarFormat,
+
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDate = selectedDay;
+              _focusedDate = focusedDay;
+            });
+          },
+
+          onPageChanged: (focusedDay) {
+            _focusedDate = focusedDay;
+          },
+
+          startingDayOfWeek: StartingDayOfWeek.monday,
+        ),
+      ],
     );
   }
 }

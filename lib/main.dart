8fe0e00b41c353f6ea604f908d2df454e82e9c3a
Nathan-slt <nathansenai22@gmail.com>
2026-09-02 13:24:05 -0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Inter',
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue, 
+          seedColor: Colors.blue,
           primary: const Color(0xFF082849),
           secondary: const Color(0XFF0371CA),
           surface: const Color(0XFFF0F7FF),
@@ -69,17 +69,15 @@ class _SplashScreenState extends State<SplashScreen> {
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  get center => null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset('assets/images/medlink.svg'),
-          ],
-        ),
+        centerTitle: true,
+        title: SvgPicture.asset('assets/images/medlink.svg'),
       ),
       body: const Center(child: Text('Em construção ...')),
     );
